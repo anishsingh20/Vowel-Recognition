e1234be9d21f978151e4b53565e8db39b12ccacc
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-10 00:58:14 +0530

    Expalnation added and loaded the packages

---
+++ mlpVowel.R
@@ -0,0 +1,8 @@
+#requireing the data and the packages to build a multi layer preceptron Model
+
+require(ElemStatLearn)#contains the dataset
+require(keras)#a deep learning package which uses tensorflow as a backend
+#mathematical computation engine .
+#tensorflow is a package which consists of data flow graphs , to perform mathematical
+#ops , where each node represents a math operator and and edge represents a tensor
+#A tensor is simply a multi-dimentional array.
