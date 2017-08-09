16a0830db95d2e514c028fc65d1653bfd7a4b652
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-10 01:06:31 +0530

    separating test and training data

--- mlpVowel.R
+++ mlpVowel.R
@@ -6,3 +6,18 @@ require(keras)#a deep learning package which uses tensorflow as a backend
 #tensorflow is a package which consists of data flow graphs , to perform mathematical
 #ops , where each node represents a math operator and and edge represents a tensor
 #A tensor is simply a multi-dimentional array.
+
+
+#checking the dimentions of test and train data
+
+cat("The dimentions of Train data set is:",dim(vowel.train))
+cat("The dimentions of Test data set is:",dim(vowel.test))
+
+#train data
+train_x<-vowel.train[-1]#the inputs or predictors
+train_y<-vowel.train[1]#the class labels of target vector
+
+head(vowel.train)
+
+#converting the data to matrix format to feed it to a MLP 
+
